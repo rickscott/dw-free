b3683802fc894284874608de1e89ab3a724a871b
shadowspar <shadowspar@dreamwidth.org>
2013-04-26 17:04:38 -0400

    mkdir $LJHOME if it isn't there already.

--- bin/bootstrap.pl
+++ bin/bootstrap.pl
@@ -32,10 +32,11 @@ my $github_user_url = "https://$GITHUB_USER\@github.com/$GITHUB_USER";
 system('bash', '-c', 'type git');
 die "I can't find git on your system -- is it installed?" unless $? == 0;
 
-# see if LJHOME is defined, and if we can go there 
+# see if LJHOME is defined, if it's present, and if we can go there
 my $LJHOME = $ENV{LJHOME};
 die "Must set the \$LJHOME environment variable before running this.\n"
     unless defined $LJHOME;
+mkdir $LJHOME unless -d $LJHOME;
 chdir( $LJHOME ) or die "Couldn't chdir to \$LJHOME directory.\n";
 
 # a .git dir in $LJHOME means dw-free is checked out. otherwise, get it
